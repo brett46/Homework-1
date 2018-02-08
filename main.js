64bdeea60b4f30697e4d501c89713c40409aa556
ashermancinelli <ashermancinelli@gmail.com>
2018-02-07 20:55:02 -0800

    fixed background image stuff

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+    
+    $('#Liam').click( function() {
+        alert('This persons name is Liam');
+    });
+    $('#Asher').click( function() {
+        alert('This persons name is Asher');
+    });
+    $('#Brett').click( function() {
+        alert('This persons name is Brett');
+    });
+
+    $('.pics').animate({
+        left: '-=500px',
+    });  
+    
+
+});
