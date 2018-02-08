9ed0fe35454212b3897d50f6b98b0f0cae4f13a5
ashermancinelli <ashermancinelli@gmail.com>
2018-02-07 23:37:13 -0800

    added slideshow animations to docs page

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 $(document).ready( function() {
+
+    let picNames = ['landingpage.png', 'Mock Screenshot 2.PNG', 'Mock Screenshot.PNG'];
+    var picNameIndex = 0;
+    var urlName = '';
+    var animateObj = {
+        'left': '+=200px',
+        'content': '',
+    }
     
     $('#Liam').click( function() {
         alert('This persons name is Liam');
@@ -14,5 +22,24 @@ $(document).ready( function() {
         left: '-=500px',
     });  
     
+    $('.slideshow-img').click(function() {
 
+        picNameIndex += 1;
+        urlName = picNames[ picNameIndex % 3 ];
+
+        $(this).animate({
+            'left': '-=100px',
+            'opacity': '0',
+        }).animate({
+            'left': '+=200px'
+        }, function() {
+            $(this).empty().append('<img class="img-slide" src="' + urlName + '" height="450" width="500"></img>');
+        }).animate({
+            'left': '-=100px',
+            'opacity': '1',
+        });
+
+    });
+    
 });
+
